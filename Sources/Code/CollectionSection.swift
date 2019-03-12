36b81ac5a8c0de696becede597bf9d65bc87ab92
au.lakomov <au.lakomov@tensor.ru>
2019-03-12 11:04:14 +0700

    1

---
+++ Sources/Code/CollectionSection.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionSection.swift
+//  TableKit
+//
+//  Created by Лакомов А.Ю. on 12/03/2019.
+//  Copyright © 2019 Max Sokolov. All rights reserved.
+//
+
+import UIKit
+
+public class CollectionSection: SheetSection {
+
+}
